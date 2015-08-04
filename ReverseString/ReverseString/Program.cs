0ab1590e9a30d2d1a871107d97645b0a12cacc66
rsmith8917 <rsmith8917@gmail.com>
2015-08-04 13:42:06 -0400

    Reverse String

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! -> " + ReverseString("Hello World!"));
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            char tmp;
+
+            for (int i = 0; i < chars.Length/2; i++)
+            {
+                tmp = chars[i];
+                chars[i] = chars[chars.Length - i - 1];
+                chars[chars.Length - i - 1] = tmp;
+            }
+
+            return new string(chars);
+        }
+    }
+}
